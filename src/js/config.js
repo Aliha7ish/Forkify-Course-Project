d68b96cd40058aede82baf87a10e272733ff114a
Aliha7ish <Alimohamedhashish@gmail.com>
2023-10-26 17:47:36 +0300

    final version

--- src/js/config.js
+++ src/js/config.js
@@ -1,7 +1,3 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIME_LIMIT = 10;
 export let key = 'f58e8dad-ceea-415c-aa66-2af6ad6ff2fb';
-console.log('new feature');
-console.log('new feature');
-console.log('new feature');
-console.log('new feature');
